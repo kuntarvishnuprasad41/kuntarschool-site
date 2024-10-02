c959679489ebda8f11c408e03d8ba1addef9b819
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-10-02 19:45:22 +0530

    About

--- src/components/template/Home/AboutSection.tsx
+++ src/components/template/Home/AboutSection.tsx
@@ -2,22 +2,22 @@ import React from "react";
 
 const AboutSection: React.FC = () => {
   return (
-    <section className=" mt-12 w-full max-w-[1763px] max-md:mt-10 max-md:max-w-full px-[62px] ">
+    <section className="mt-0 lg:mt-12 w-full max-w-[1763px] max-md:mt-10 max-md:max-w-full px-[62px] ">
       <div className="flex gap-5 max-md:flex-col">
         <div className="flex flex-col w-[55%] justify-center items-center max-md:ml-0 max-md:w-full">
           <img
             loading="lazy"
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/a401bbff11d1a42e4d49bf886defd18b75f7774f98b62da79c5605f8e5585ac0?placeholderIfAbsent=true&apiKey=69b6bc2ce7e445dc86378964c554c673"
             alt="Children playing"
-            className="object-contain w-full aspect-[1.25] max-md:mt-10 max-md:max-w-full"
+            className="object-contain w-full aspect-[1.0] max-md:mt-5 max-md:max-w-full"
           />
         </div>
-        <div className="flex flex-col ml-5 w-[45%] max-md:ml-0 max-md:w-full">
-          <div className="flex flex-col items-start mt-6 w-full max-md:mt-10 max-md:max-w-full">
+        <div className="flex flex-col   w-[45%] max-md:ml-0 max-md:w-full">
+          <div className="flex flex-col items-start mt-0 w-full max-md:mt-0 max-md:max-w-full">
             <h2 className="text-5xl font-bold tracking-wider leading-none text-black max-md:text-4xl">
               Our Programs
             </h2>
-            <p className="self-stretch mt-14 text-2xl tracking-wider leading-10 text-black max-md:mt-10 max-md:max-w-full">
+            <p className="self-stretch mt-0 text-2xl tracking-wider leading-10 text-black max-md:mt-10 max-md:max-w-full">
               Being brave isn't always a grand gesture sometimes it just means
               having a go attempting that difficult question, offering an answer
               in a lesson when you're simply really trying new.
